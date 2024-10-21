b924b5a08d08ba516903a266dd2487d073d3a054
jjennyvc <nyho100>
2024-10-21 11:05:36 -0700

    skibidi toilet

--- Coins.java
+++ Coins.java
@@ -2,9 +2,35 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    public static void coinConverter(int pennies) {
+
+      int dollars = pennies / 100;
+      pennies %= 100;
+
+      int quarters = pennies / 25;
+      pennies %= 25;
+
+      int dimes = dimes / 10;
+      dimes %= 10;
+
+      int nickles = nickles / 5;
+      nickles %= 5;
+
+      int remainingPennies = pennies;
+
+      System.out.println("Dollar Bills: " + dollars);
+      System.out.println("Quaters: " + quarters);
+      System.out.println("Dimes: " + dimes);
+      System.out.println("Nickles: " + nickles);
+      System.out.println("Pennies: " + remainingPennies);
+
+
+    }
+   
+   
+
   }
 
-  // write code for coinConverter
+  
 
 }
